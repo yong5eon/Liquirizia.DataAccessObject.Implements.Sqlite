e56366e21ba9565833b45fe827c790bc79cc8f7a
허용선 <yongseon.heo@gmail.com>
2024-07-26 02:48:21 +0900

    테스트 파일 추가

--- test/Table.py
+++ test/Table.py
@@ -1,16 +1,38 @@
-from Liquirizia.DataAccessObject.Implements.Sqlite.Properties import *
 from Liquirizia.DataAccessObject.Implements.Sqlite.Types import *
+from Liquirizia.DataAccessObject.Implements.Sqlite.Properties import *
+from Liquirizia.DataAccessObject.Implements.Sqlite.Executors import *
+
 from Liquirizia.DataModel import Model
 
 from Liquirizia.Util import PrettyPrint, PrettyDump
 
-@Table('SampleTable')
-class SampleModel(Model):
+
+@Table(
+	name = 'STUDENT',
+	primaryKey=PrimaryKey('ID'),
+	indexes=(
+		IndexUnique('IDX_UNIQUE_STUDENT_CODE', columns='CODE'),
+		Index(name='IDX_STUDENT_IS_DELETED', columns='IS_DELETED'),
+		Index(name='IDX_STUDENT_AT_CREATED', columns='AT_CREATED'),
+		Index(name='IDX_STUDENT_AT_UPDATED', columns='AT_UPDATED', null='LAST'),
+	)
+)
+class Student(Model):
 	id = Integer('ID', autoincrement=True)
-	pass
+	code = Text('CODE')
+	name = Text(name='NAME')
+	# metadata = Buffer(name='METADATA')
+	atCreated = DateTime(name='AT_CREATED', default='"NOW"')
+	atUpdated = Timestamp(name='AT_UPDATED', null=True)
+	isDeleted = Text(name='IS_DELETED', default='"N"', check='IS_DELETED IN ("Y", "N")')
 
-o = Model()
-
-for sql in Model.ToString():
+for sql in Create(Student):
+	PrettyPrint(sql)
 	print(sql)
+
+o = Student(
+	id=1,
+	code='18238232',
+)
 PrettyPrint(o)
+print(o)
