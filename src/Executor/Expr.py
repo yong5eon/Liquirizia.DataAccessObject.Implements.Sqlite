f532b14adc50b7c45edc08d5508f971727ece982
허용선 <yongseon.heo@gmail.com>
2024-08-05 01:24:50 +0900

    필터를 표현식으로 변경

---
+++ src/Executor/Expr.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+
+__all__ = (
+	'Expr'
+)
+
+
+class Expr(metaclass=ABCMeta):
+	"""Filter Interface"""
+
+	@abstractmethod
+	def __str__(self):
+		raise NotImplementedError('{} must be implemented __str__'.format(self.__class__.__name__))
