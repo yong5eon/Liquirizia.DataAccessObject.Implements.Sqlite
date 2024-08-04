6be26beac21db65e05c1399a8cbfd677e7b21463
허용선 <yongseon.heo@gmail.com>
2024-08-05 01:28:52 +0900

    조건 필터 표현식 추가

---
+++ src/Executor/Filter/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from .IsEqualTo import IsEqualTo
+from .IsNotEqualTo import IsNotEqualTo
+from .IsLike import IsLike
+from .IsGreaterThan import IsGreaterThan
+from .IsGreaterEqualTo import IsGreaterEqualTo
+from .IsLessThan import IsLessThan
+from .IsLessEqualTo import IsLessEqualTo
+
+__all__ = (
+	'IsEqualTo',
+	'IsNotEqualTo',
+	'IsLike',
+	'IsGreaterThan',
+	'IsGreaterEqualTo',
+	'IsLessThan',
+	'IsLessEqualTo',
+)
