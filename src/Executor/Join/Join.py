b0998d4442c26cfae0bcd5a551dbfb1a546ae2bc
허용선 <yongseon.heo@gmail.com>
2024-08-05 01:28:35 +0900

    조인 표현식 추가

---
+++ src/Executor/Join/Join.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from ..Expr import Expr
+
+from ...Model import Table
+
+__all__ = (
+	'Join'
+)
+
+
+class Join(Expr):
+	"""Join Class"""
+
+	def __init__(self, expr: str, table: Table, *args: type[list[Expr]]) -> None:
+		self.expr = expr
+		self.table = table
+		self.args = args
+		return
+
+	def __str__(self):
+		return '{} JOIN {} ON {}'.format(
+			self.expr,
+			self.table.__properties__['name'],
+			' AND '.join([str(arg) for arg in self.args]),
+		)
---
+++ src/Executor/Join/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from .Join import Join
+
+from .Inner import Inner
+from .LeftOuter import LeftOuter
+from .RightOuter import RightOuter
+from .FullOuter import FullOuter
+
+__all__ = (
+	'Join',
+	'Inner',
+	'LeftOuter',
+	'RightOuter',
+	'FullOuter',
+)
