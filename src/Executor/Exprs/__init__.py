02d3bf99c25a79ae5ed3da323a0f5622b0c892ff
허용선 <yongseon.heo@gmail.com>
2024-08-05 01:29:02 +0900

    일반 표현식 추가

---
+++ src/Executor/Exprs/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from .Alias import Alias
+
+from .Sum import Sum
+from .Count import Count
+from .Average import Average
+
+__all__ = (
+	'Alias',
+	'Sum',
+	'Count',
+	'Average',
+)
