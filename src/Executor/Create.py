d9943e007e5935f83bc52ee87eee51a259c1b5e9
허용선 <yongseon.heo@gmail.com>
2024-08-09 02:27:32 +0900

    View 생성 추가

--- src/Executor/Create.py
+++ src/Executor/Create.py
@@ -1,48 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from Liquirizia.DataModel import Model
 from Liquirizia.DataAccessObject.Model import Executors
 
-from ..Model import Table
+from ..Model import Type as ModelType
 from ..Type import Type
 from ..Constraint import PrimaryKey,ForeignKey
 from ..Index import Index, IndexUnique
-
-from typing import overload
 
 __all__ = (
 	'Create'
 )
 
 
-class Create(Executors):
-	def __init__(self, o: type[Table], notexist: bool = False):
-		self.model = o
-		self.executors = []
-		_ = []
-		for k, v in o.__dict__.items():
-			if isinstance(v, Type):
-				_.append(self.toStringAttribute(v))
-		if o.__properties__['primaryKey']:
-			_.append(self.toStringPrimaryKey(o.__properties__['primaryKey']))
-		_.extend([self.toStringForeignKey(foreignKey) for foreignKey in o.__properties__['foreignKeys']] if o.__properties__['foreignKeys'] else [])
-		self.executors.append(('CREATE TABLE {}{}({})'.format(
-			' IF NOT EXISTS ' if notexist else '',
-			o.__properties__['name'],
-			', '.join(_)
-		), ()))
-		for index in o.__properties__['indexes'] if o.__properties__['indexes'] else []:
-			if isinstance(index, IndexUnique):
-				self.executors.append((self.toStringIndexUnique(index), ()))
-				continue
-			if isinstance(index, Index):
-				self.executors.append((self.toStringIndex(index), ()))
-				continue
-		return
-	
-	def __iter__(self):
-		return self.executors.__iter__()
-
-	def toStringAttribute(self, attr: Type):
+class TypeToSQL(object):
+	def __call__(self, attr:Type) -> str:
 		return '{} {}{}{}{}{}{}{}'.format(
 			attr.key,
 			attr.type,
@@ -54,19 +26,22 @@ class Create(Executors):
 			' REFERENCES {}({})'.format(attr.referenceTable, attr.referenceKey) if attr.reference else '',
 		)
 
-	def toStringPrimaryKey(self, primaryKey: PrimaryKey):
+class PrimaryKeyToSQL(object):
+	def __call__(self, key: PrimaryKey) -> str:
 		return 'PRIMARY KEY({})'.format(
-			', '.join(primaryKey.columns)
+			', '.join(key.columns)
 		)
 
-	def toStringForeignKey(self, foreignKey: ForeignKey):
+class ForeignKeyToSQL(object):
+	def __call__(self, key: ForeignKey) -> str:
 		return 'FOREIGN KEY({}) REFERENCES {}({})'.format(
-			', '.join(foreignKey.columns),
-			foreignKey.table,
-			', '.join(foreignKey.references)
+			', '.join(key.columns),
+			key.table,
+			', '.join(key.references)
 		)
 
-	def toStringIndex(self, index: Index):
+class IndexToSQL(object):
+	def __call__(self, index: Index) -> str:
 		return 'CREATE INDEX {}{} ON {}({}){}'.format(
 			'IF NOT EXISTS ' if index.notexists else '',
 			index.name,
@@ -74,8 +49,9 @@ class Create(Executors):
 			', '.join(index.colexprs),
 			' WHERE {}'.format(index.expr) if index.expr else '',
 		)
-	
-	def toStringIndexUnique(self, index: IndexUnique):
+
+class IndexUniqueToSQL(object):
+	def __call__(self, index: IndexUnique) -> str:
 		return 'CREATE UNIQUE INDEX {}{} ON {}({}){}'.format(
 			'IF NOT EXISTS ' if index.notexists else '',
 			index.name,
@@ -83,3 +59,66 @@ class Create(Executors):
 			', '.join(index.colexprs),
 			' WHERE {}'.format(index.expr) if index.expr else '',
 		)
+
+class TableToSQL(object):
+
+	TypeToSQL = TypeToSQL()
+	PrimaryKeyToSQL = PrimaryKeyToSQL()
+	ForeignKeyToSQL = ForeignKeyToSQL()
+	IndexToSQL = IndexToSQL()
+	IndexUniqueToSQL = IndexUniqueToSQL()
+
+	def __call__(self, o: type[Model], notexist) -> str:
+		__ = []
+		_ = []
+		for k, v in o.__dict__.items():
+			if isinstance(v, Type):
+				_.append(self.TypeToSQL(v))
+		if o.__properties__['primaryKey']:
+			_.append(self.PrimaryKeyToSQL(o.__properties__['primaryKey']))
+		_.extend([self.ForeignKeyToSQL(foreignKey) for foreignKey in o.__properties__['foreignKeys']] if o.__properties__['foreignKeys'] else [])
+		__.append(('CREATE TABLE {}{}({})'.format(
+			'IF NOT EXISTS ' if notexist else '',
+			o.__properties__['name'],
+			', '.join(_)
+		), ()))
+		for index in o.__properties__['indexes'] if o.__properties__['indexes'] else []:
+			if isinstance(index, IndexUnique):
+				__.append((self.IndexUniqueToSQL(index), ()))
+				continue
+			if isinstance(index, Index):
+				__.append((self.IndexToSQL(index), ()))
+				continue
+		return __
+	
+
+class ViewToSQL(object):
+	def __call__(self, o: type[Model], notexist) -> str:
+		return [('CREATE VIEW {}{} AS {}'.format(
+			'IF NOT EXISTS ' if notexist else '',
+			o.__properties__['name'],
+			o.__properties__['executor'].query,
+		), ())]
+
+	
+
+class Create(Executors):
+
+	TableToSQL = TableToSQL()
+	ViewToSQL = ViewToSQL()
+
+	def __init__(self, o: type[Model], notexist: bool = True):
+		self.model = o
+		self.executors = []
+		fn = {
+			ModelType.Table : Create.TableToSQL,
+			ModelType.View  : Create.ViewToSQL,
+		}.get(o.__properties__['type'], None)
+		if fn:
+			self.executors = fn(o,  notexist)
+		return
+	
+	def __iter__(self):
+		return self.executors.__iter__()
+	
+
