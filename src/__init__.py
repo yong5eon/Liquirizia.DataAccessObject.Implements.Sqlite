4ed5886c417618f92ba05648404077963f0b415f
허용선 <yongseon.heo@gmail.com>
2024-09-13 01:10:55 +0900

    datetime 아답터 추가

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from typing import Any
 from .Configuration import Configuration
 from .Connection import Connection
 from .Cursor import Cursor
 from .Session import Session
 from .Context import Context
+
+from sqlite3 import register_adapter
+from datetime import datetime
 
 __all__ = (
 	'Configuration',
@@ -13,3 +17,10 @@ __all__ = (
 	'Session',
 	'Context',
 )
+
+
+class DateTimeAdator(object):
+	def __call__(self, o: datetime):
+		return o.isoformat()
+
+register_adapter(datetime, DateTimeAdator())
